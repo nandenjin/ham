8ceba88fbe20b2ecf4fb0f8b9b2ce76220602f4b
Kazumi Inada <timer.ground.gm@gmail.com>
2019-05-14 09:47:59 +0900

    Add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,16 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Kazumi Inada / Hirasuna Art Movement',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }
     ]
   },
 
