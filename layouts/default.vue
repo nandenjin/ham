c93c7f058fb3bfd87cce54734d0a7ecc98c1aa92
Kazumi Inada <hello@nandenjin.com>
2020-12-23 16:54:27 +0900

    feat: Add finished message

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="layout-wrap">
-    <the-side-bar class="sidebar" />
-    <div class="content">
-      <nuxt />
+  <div>
+    <div class="finished">
+      <strong>本展は2019年6月をもって終了しました。</strong>
+      <span>ご来場ありがとうございました。</span>
+      <a href="https://www.nandenjin.com/news/200124_making-notes" target="_blank">関連情報: メイキング記事公開のお知らせ</a>
+    </div>
+    <div class="layout-wrap">
+      <the-side-bar class="sidebar" />
+      <div class="content">
+        <nuxt />
+      </div>
     </div>
   </div>
 </template>
@@ -18,12 +25,25 @@
 </script>
 
 <style lang="sass">
+  .finished
+    padding: 10px 20px
+    background-color: #fff
+    color: #111
+    font-font-feature-settings: 'palt'
+    font-size: 13px
+
+    a
+      color: inherit
+      text-decoration: underline
 
   .layout-wrap
     .sidebar
-      position: fixed
+      position: sticky
       top: 0
       left: 0
       z-index: 100
+      float: left
 
+    .content
+      padding: .1px 0
 </style>
